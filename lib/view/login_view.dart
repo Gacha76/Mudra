25908439a9cd51bafd8efcdfe2e334f7761a5cf0
sharan <tsharan76@gmail.com>
2023-03-26 09:54:02 +0530

    Ch-21 done

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,7 +1,7 @@
 import 'package:classico/constants/routes.dart';
+import 'package:classico/utilities/show_error_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   //'right-click + rename'
@@ -73,14 +73,14 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log("User not found");
+                  await showErrorDialog(context, "User not found");
                 } else if (e.code == 'wrong-password') {
-                  devtools.log("Wrong password");
+                  await showErrorDialog(context, "Wrong credentials");
+                } else {
+                  await showErrorDialog(context, "Error ${e.code}");
                 }
               } catch (e) {
-                devtools.log("Something bad happened");
-                devtools.log(e.runtimeType.toString());
-                devtools.log(e.toString());
+                await showErrorDialog(context, e.toString());
               }
             },
             child: const Text("Login"),
